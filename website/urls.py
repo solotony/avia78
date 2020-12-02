38f489babc5bdb8d2ab3f4456984039b64e1e31e
Antonio Solo <as@solotony.com>
2020-12-02 11:13:09 +0300

    developer pages

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.front_page, name='website.front'),
+    re_path('^solotony/$', TemplateView.as_view(template_name='website/developer.html'), name='developer'),
+    re_path(r'^cmsmagazinec4639f3b000df051ff472e1a11bfb147\.txt$',
+            TemplateView.as_view(template_name='website/cmsmagazine.txt', content_type='text/plain')),
 ]
